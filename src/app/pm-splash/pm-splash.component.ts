fb578143d8836420bb0fe2bfe6edb6ae1eb56d08
Saravanakumar Radhakrishnan <saravanakumar_radhakrishnan@trimble.com>
2020-09-20 20:54:38 +0530

    until flags are implemented at runtime

--- src/app/pm-splash/pm-splash.component.ts
+++ src/app/pm-splash/pm-splash.component.ts
@@ -3,13 +3,65 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pm-splash',
   templateUrl: './pm-splash.component.html',
-  styleUrls: ['./pm-splash.component.css']
+  styleUrls: ['./pm-splash.component.css'],
 })
 export class PmSplashComponent implements OnInit {
+  initialLeftPosition: 100;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  getFlagStyle(leftPx: string) {
+    const flagStyle = {
+      left: leftPx,
+    };
+    return flagStyle;
   }
 
+  getMonthPosition(leftPx: string) {
+    const monthStyle = {
+      left: leftPx,
+    };
+    return monthStyle;
+  }
+
+  get PmDuePositions(): Array<number> {
+    return [400, 500, 550];
+  }
+
+  get MonthNames(): Array<string> {
+    const monthNames = [
+      'Dec',
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+    ];
+    const currentMonth = new Date().getMonth();
+    return [
+      monthNames[
+        currentMonth - 2 < 0
+          ? 12 - Math.abs(currentMonth - 2)
+          : currentMonth - 2
+      ],
+      monthNames[
+        currentMonth - 1 < 0
+          ? 12 - Math.abs(currentMonth - 1)
+          : currentMonth - 1
+      ],
+      monthNames[currentMonth],
+      monthNames[(currentMonth + 1) % 12],
+      monthNames[(currentMonth + 2) % 12],
+      monthNames[(currentMonth + 3) % 12],
+      monthNames[(currentMonth + 4) % 12],
+    ];
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
